9023786c942fa89c239380de647c56ca2376bdac
Erli <85597743+Erli-Erli@users.noreply.github.com>
2022-01-18 10:20:34 +0700

    up

--- app/src/main/java/ac/id/atmaluhur/mhs_pedulilindungi/RiwayatActivity.java
+++ app/src/main/java/ac/id/atmaluhur/mhs_pedulilindungi/RiwayatActivity.java
@@ -2,13 +2,28 @@ package ac.id.atmaluhur.mhs_pedulilindungi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RiwayatActivity extends AppCompatActivity {
 
+    private Button btnback;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_riwayat);
+
+        btnback = findViewById(R.id.btnBackHis);
+        btnback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(RiwayatActivity.this,dasbordActivity.class );
+                startActivity(i);
+
+            }
+        });
+
     }
 }
--- app/src/main/java/ac/id/atmaluhur/mhs_pedulilindungi/dasbordActivity.java
+++ app/src/main/java/ac/id/atmaluhur/mhs_pedulilindungi/dasbordActivity.java
@@ -2,13 +2,29 @@ package ac.id.atmaluhur.mhs_pedulilindungi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class dasbordActivity extends AppCompatActivity {
+
+    private Button btnRiwayat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dasbord);
+
+
+        btnRiwayat = findViewById (R.id.btnarrow);
+        btnRiwayat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(dasbordActivity.this,RiwayatActivity.class );
+                startActivity(i);
+
+            }
+        });
     }
 }
