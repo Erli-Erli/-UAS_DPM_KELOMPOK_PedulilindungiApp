26579c5a525cb77b89168b89db2eb4c752fbbc5b
Erli <85597743+Erli-Erli@users.noreply.github.com>
2022-01-18 13:26:37 +0700

    up

--- app/src/main/java/ac/id/atmaluhur/mhs_pedulilindungi/login_Activity.java
+++ app/src/main/java/ac/id/atmaluhur/mhs_pedulilindungi/login_Activity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class dasbordActivity extends AppCompatActivity {
+public class login_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dasbord);
+        setContentView(R.layout.activity_login2);
     }
 }
