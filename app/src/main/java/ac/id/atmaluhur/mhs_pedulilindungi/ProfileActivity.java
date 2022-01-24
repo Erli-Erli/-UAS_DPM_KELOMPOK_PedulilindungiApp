3fdfab2ec274ab2ee8019a3d15c4e8520de60c4f
Erli <85597743+Erli-Erli@users.noreply.github.com>
2022-01-24 12:48:38 +0700

    up

--- app/src/main/java/ac/id/atmaluhur/mhs_pedulilindungi/ProfileActivity.java
+++ app/src/main/java/ac/id/atmaluhur/mhs_pedulilindungi/ProfileActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 public class ProfileActivity extends AppCompatActivity {
 
     private Button backprofil;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
